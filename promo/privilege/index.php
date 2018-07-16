51219098a9468efcc9c5fa4df4d9ad37b90b4e34
neeil1990 <neeil@mail.ru>
2018-07-16 14:21:20 +0300

    no message

--- promo/privilege/index.php
+++ promo/privilege/index.php
@@ -3,9 +3,10 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("КАСКО 3,8% + пакет Привилегия");
 ?>
 
+
     <!--
-    Контент страницы начнется тут
-    -->
+  Контент страницы начнется тут
+  -->
 
     <main class="main" data-model="home">
 
@@ -31,9 +32,9 @@ $APPLICATION->SetTitle("КАСКО 3,8% + пакет Привилегия");
 
                 <div class="offers-content">
 
-                    <h1 class="title-offer "><span class="nowrp">КАСКО 3,8% + пакет "Привилегия"</span></h1>
+                    <h1 class="title-offer bordr-btm"><span class="nowrp">КАСКО 3,8% + пакет "Привилегия"</span></h1>
 
-                    <div class="offer-terms ">
+                    <div class="offer-terms bordr-btm">
                         <div class="offers-block">
                             <div class="row-inline clearfix">
                                 <!--<h2 class="title-offer__lev02 multiple-cars">КАСКО 3,8% + пакет "Привилегия"</h2>-->
@@ -45,6 +46,34 @@ $APPLICATION->SetTitle("КАСКО 3,8% + пакет Привилегия");
                                 </div>
 
 
+                                <div class="offer-actions clearfix">
+                                    <div class="row row-left">
+                                        <img src="http://api.cofigurator.hyundai.ru/images/car_photo_exterior360/Sonata_LF_Grand_Blue_afcdde9626d976bc331aec9b108e83e4/13.png" alt="Hyundai Sonata" width="440" height="180">
+                                    </div>
+                                    <div class="row row-right">
+                                        <div class="offer-actions__button-span">
+                                            <a href="http://www.hyundai.ru/configurator/car/24/modification" class="offer-actions__button config">Конфигуратор</a>
+
+
+
+                                            <div id="brochure_requester"><a data-gtm-havas-model="sonata" href="#" data-model="Sonata" for="brochures_request"  target="_blank" class="zing_form_toggler offer-actions__button brosh">Брошюра</a></div>
+                                        </div>
+
+
+
+
+                                    </div>
+
+
+
+
+                                    <div class="offer-actions__button-span">
+                                        <a href="http://www.hyundai.ru/configurator/car/24/modification" class="offer-actions__button credit">Кредит</a>
+                                        <a href="/test-drive/" class="offer-actions__button testdrive">Тест-драйв</a>
+                                    </div>
+                                    <a class="offer-actions__description" href="/Sonata/">Описание модели ></a>
+                                </div>
+
 
                                 <!-- additional terms as footer -->
 
@@ -55,12 +84,9 @@ $APPLICATION->SetTitle("КАСКО 3,8% + пакет Привилегия");
                                 <div class="tip-supscript ">
                                     <p style="font-size: 10px;">
                                         *«КАСКО 3,8%» − программа добровольного страхования нового автомобиля Hyundai Sonatа в САО «ВСК».<br>
-                                        Договор заключается на один год и покрывает риски повреждения, в том числе полной гибели, а также хищения автомобиля, с безусловной франшизой в размере 25 000 руб.
-                                        Обмен автомобиля осуществляется с соблюдением условий и ограничений, указанных в страховом полисе.<br>
                                         Подробности в салонах официальных дилеров HYUNDAI.<br>
-                                        Предложение действительно с 01.02.2018г. до 28.02.2018г., носит информационный характер, не является публичной офертой (ст. 437 ГК РФ). Условия программы могут быть изменены в одностороннем порядке. Услуги по страхованию оказывает САО «ВСК», лицензии ЦБ РФ - СЛ № 0621, СИ № 0621 от 11.09.2015. www.vsk.ru.<br>
+                                        Предложение действительно с 01.06.2018г. по 30.06.2018г., носит информационный характер, не является публичной офертой (ст. 437 ГК РФ). Условия программы могут быть изменены в одностороннем порядке. Услуги по страхованию оказывает САО «ВСК», лицензии ЦБ РФ - СЛ № 0621, СИ № 0621 от 11.09.2015. www.vsk.ru.<br>
                                         Пакет "Привилегия" доступен не только с тарифом 3,8%, но и с любым продуктом полного КАСКО от СК ВСК.
-
                                     </p>
                                 </div>
 
