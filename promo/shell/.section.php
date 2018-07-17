e65befa2212c528c4d62ab152672acc80990738d
neeil1990 <neeil@mail.ru>
2018-07-17 17:18:03 +0300

    no message

---
+++ promo/shell/.section.php
@@ -0,0 +1,6 @@
+<?
+$sSectionName = "shell";
+$arDirProperties = Array(
+
+);
+?>
---
+++ promo/shell/index.php
@@ -0,0 +1,276 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новые рекорды с сервисом Hyundai!");
+?>
+    <!--
+    Контент страницы начнется тут
+    -->
+
+    <main class="main" data-model="home">
+
+        <style>
+            #wrapper {
+                background: #f5f5f5 !important;
+            }
+        </style>
+
+        <div class="offers-container">
+            <div class="content_area">
+                <!-- hide elantra and ix35 banner-->
+
+                <!-- WARNING -->
+                <!-- this checks only img src!! no checking by offer ID -->
+
+                <img class="promo-head" src="http://www.hyundai.ru/media/offers_image/94402c65b808ea31122ce0c19f8d99e9_1920x530_Hyundai_Shell (2).jpg" style=""/>
+
+                <!-- old syntax: -->
+                <!-- <img class="promo-head" src="http://www.hyundai.ru/media/offers_image/dd77b6edbce77f33b7c4c23fd7d6e125_1920x530_Hyundai_Shell.jpg" style=""/> -->
+
+                <!-- end of hiding banner-->
+
+                <div class="offers-content">
+
+                    <div class="offer-terms bordr-btm">
+                        <div class="offers-block">
+                            <div class="row-inline">
+                                <h2 class="title-offer__lev02 multiple-cars">Новые рекорды с сервисом Hyundai!</h2>
+
+
+
+
+                                <p class="offer-terms__text">За годы инновационного сотрудничества Hyundai и «Шелл» делали всё, чтобы реализовать возможности и обеспечить защиту каждого двигателя Hyundai.  И для самых взыскательных клиентов совместно с компанией Шелл мы запустили специальную линейку моторных масел Shell Helix Ultra ECT AH 5W-30.</p>
+
+                                <p class="offer-terms__text">Моторное  масло Shell Helix Ultra ECT AH 5W-30  обладает особыми эксплуатационными характеристиками, поскольку  разработано из проверенных высокоэффективных компонентов  с применением технологии Shell PurePlus и  создано специально для автомобилей Hyundai.  <a href="https://www.hyundai.ru/shell" target="_blank">Подробнее</a></p>
+
+                                <!--<p class="offer-terms__text">Преимущества масла Shell Helix Ultra ECT AH 5W-30
+                                <ul>
+                                <li>- Высокая степень чистоты поршня</li>
+                                <li>- Улучшенная топливная экономичность</li>
+                                <li>- Надежная защита от износа</li>
+                                <li>- Высокий уровень антикоррозийных свойств</li>
+                                <li>- Защита от образования различных отложений</li>
+                                <li>- Легкий пуск двигателя при низких температурах, даже при –40 ˚C *</li>
+                                </ul>
+                                </p><br/>-->
+
+                                <p class="offer-terms__text">Мы знаем, что владельцы Hyundai заинтересованы в надежной и эффективной работе двигателей своих автомобилей, поэтому приглашаем посетить официальные дилерские центры Hyundai, где вы сможете :
+                                <ul>
+                                    <li>- пройти бесплатную диагностику по программе «Лучшее для своих»**</li>
+                                    <li>- воспользоваться услугой замены масла на Shell Helix Ultra ECT AH 5W-30  по специальной цене:</li>
+                                </ul>
+                                </p><br/>
+
+                                <table cellpadding="10" cellspacing="0" border="1" style="margin-bottom: 20px;">
+                                    <tr>
+                                        <th style="text-align: center !important;">Модель</th>
+                                        <th style="text-align: center !important;">Стоимость, руб с НДС***</th>
+                                    </tr>
+                                    <tr>
+                                        <td style="text-align: center !important;">Solaris</td>
+                                        <td style="text-align: center !important;">2499</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="text-align: center !important;">ix 35; Creta; </td>
+                                        <td style="text-align: center !important;">2999</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="text-align: center !important;">Elantra; i-40; i-30; Sonata</td>
+                                        <td style="text-align: center !important;">2999</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="text-align: center !important;">Tucson; Santa Fe****</td>
+                                        <td style="text-align: center !important;">3499</td>
+                                    </tr>
+                                </table>
+
+
+                                <p class="offer-terms__text">Акция действует до 31 августа 2018. Торопитесь!</p>
+                                <p class="offer-terms__text">Подробности кампании Вы можете уточнить у официальных дилеров Hyundai или по телефону: 8 800 333-71-67 </p>
+                            </div>
+
+                            <!-- additional terms as footer -->
+
+                            <div class="clearfix"></div>
+
+
+
+                            <div class="tip-supscript ">
+                                <p style="font-size: 10px;">
+                                    *только для моторных масел класса вязкости «0W-XX». Подробности на сайте <a href="https://www.hyundai.ru/shell">https://www.hyundai.ru/shell</a>
+                                </p>
+
+                                <p style="font-size: 10px;">
+                                    ** Сервисная программа «Лучшее для своих» включает в себя комплексную бесплатную диагностику автомобилей Hyundai старше 2-х лет с момента их выпуска по 36 ключевым пунктам без ограничений по сроку эксплуатации и пробегу автомобилей (в рамках кампании услуга предоставляется для автомобилей любого возраста).
+                                </p>
+
+                                <p style="font-size: 10px;">
+                                    *** Рекомендованные максимальные розничные цены. В стоимость включены работы по замене масла, оригинальный масляный фильтр, необходимое количество масла Shell Helix Ultra ECT AH 5W-30, согласно рекомендациям в сервисной книжке автомобиля.
+                                </p>
+
+                                <p style="font-size: 10px;">
+                                    **** В кампании участвуют автомобили Tucson, Santa Fe только с бензиновыми моторами.
+                                </p>
+
+                            </div>
+
+                            <div class="clearfix"></div>
+                        </div>
+
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+
+
+            <div class="bottom-area-line">
+                <div class="bottom-area-line__inner grid">
+                    <div class="grid-el grid-el-50 bottom-area-line__inner-left">
+                        <div class="main-message">Мы хотим, чтобы владеть автомобилем Hyundai было просто и удобно</div>
+                    </div>
+                    <div class="grid-el grid-el-50 bottom-area-line__inner-right">
+                        <a href="/test-drive/" class="request-service-btn" style="margin-right: 20px;">запись на сервис</a>
+                        <a href="/shell/" class="request-service-btn">Выберите масло</a>
+                    </div>
+                </div>
+            </div>
+
+            <style>
+                .bottom-area-line {
+                    background: #1d88ce none repeat scroll 0 0;
+                }
+                .bottom-area-line__inner {
+                    margin: 0 auto;
+                    padding: 40px 0;
+                    width: 940px;
+                }
+                .bottom-area-line__inner-right {
+                    text-align: right;
+                }
+                .main-message {
+                    color: #fff;
+                    font-family: "HyundaiSans";
+                    font-size: 24px;
+                    font-weight: normal;
+                    line-height: 30px;
+                    width: 470px;
+                }
+                .bottom-area-line__inner-right {
+                    padding-top: 5px;
+                }
+                .offers-link {
+                    color: #fff;
+                    display: inline-block;
+                    font-family: "Arial",sans-serif;
+                    font-size: 13px;
+                    font-weight: bold;
+                    letter-spacing: 0.3px;
+                    margin-left: 50px;
+                    text-transform: uppercase;
+                    transition: opacity 0.15s ease 0s;
+                    vertical-align: middle;
+                }
+                .offers-link:hover {
+                    opacity: 0.75;
+                    text-decoration: none;
+                }
+                .request-service-btn {
+                    display: inline-block;
+                }
+
+                .request-service-btn {
+                    -moz-user-select: none;
+                    background: transparent none repeat scroll 0 0;
+                    border: 2px solid #fff;
+                    color: #fff;
+                    display: inline-block;
+                    font-family: "Arial",sans-serif;
+                    font-size: 13px;
+                    font-weight: bold;
+                    padding: 16px 29px;
+                    text-align: center;
+                    text-transform: uppercase;
+                    transition: background 0.15s cubic-bezier(0.18, 0.89, 0.32, 1.28) 0s, border-color 0.15s cubic-bezier(0.18, 0.89, 0.32, 1.28) 0s, color 0.15s cubic-bezier(0.18, 0.89, 0.32, 1.28) 0s;
+                }
+                .request-service-btn:hover {
+                    background: #fff none repeat scroll 0 0;
+                    color: #1d88ce;
+                    text-decoration: none;
+                }
+
+                .offers-link {
+                    color: #fff;
+                    display: inline-block;
+                    font-family: "Arial",sans-serif;
+                    font-size: 13px;
+                    font-weight: bold;
+                    letter-spacing: 0.3px;
+                    margin-left: 50px;
+                    text-transform: uppercase;
+                    transition: opacity 0.15s ease 0s;
+                    vertical-align: middle;
+                }
+                .offers-link:hover {
+                    opacity: 0.75;
+                    text-decoration: none;
+                }
+                .grid .grid-el-50 {
+                    width: 50%;
+                }
+                .grid .grid-el {
+                    float: left;
+                    position: relative;
+                }
+
+                .grid::after {
+                    clear: both;
+                    content: ".";
+                    display: block;
+                    height: 0;
+                    line-height: 0;
+                    visibility: hidden;
+                }
+                .grid {
+                    position: relative;
+                }
+                .offer-terms__text {
+                    margin-bottom: 20px;
+                }
+                .offers-block li {
+                    font-size: 16px;
+                }
+
+                table{
+                    width: unset;
+                }
+
+                .offers-block td, .offers-block th {
+                    font-size: 16px;
+                    padding: 10px;
+                    border: 1px solid;
+                }
+                .offers-block th {
+                    font-weight: bold;
+                }
+            </style>
+        </div>
+        </div>
+
+        <!--RuTarget-->
+        <script type="text/javascript">
+            var _rutarget=window._rutarget||[];
+            _rutarget.push({
+                'event':'otherPage',
+                'nosync': 'true'
+            });
+        </script>
+        <!--/RuTarget-->
+    </main>
+
+
+    <!--
+    Контент страницы закончится тут
+    -->
+
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
