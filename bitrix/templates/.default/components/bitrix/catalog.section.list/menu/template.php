ee71f5f75d032440442798290f771e896e3aa121
neeil1990 <neeil@mail.ru>
2018-08-23 11:49:41 +0300

    no message

--- bitrix/templates/.default/components/bitrix/catalog.section.list/menu/template.php
+++ bitrix/templates/.default/components/bitrix/catalog.section.list/menu/template.php
@@ -53,15 +53,13 @@ $arSectionDeleteParams = array("CONFIRM" => GetMessage('CT_BCSL_ELEMENT_DELETE_C
 	<li class="showroom__cat" role="menuitem">
 		<div class="showroom__cat-title"><?=$arSection['NAME']?></div>
 		<div class="showroom__cat-items">
-
 			<?
 			$arSelect = Array("ID", "IBLOCK_ID","CODE","PREVIEW_PICTURE", "NAME","PROPERTY_PRICE","PROPERTY_PROMO");
 			$arFilter = Array("IBLOCK_ID" => $arSection['IBLOCK_ID'],"SECTION_ID" => $arSection['ID'],"ACTIVE"=>"Y");
-			$res = CIBlockElement::GetList(Array(), $arFilter, false, Array(), $arSelect);
+			$res = CIBlockElement::GetList(Array("SORT"=>"ASC"), $arFilter, false, Array(), $arSelect);
 			while($ob = $res->GetNextElement()):
 			$arFields = $ob->GetFields();
 			?>
-
 			<!--showroom__cat-item--promo-->
 			<a href="/<?=$arFields['CODE']?>" class="showroom__cat-item <?if($arFields['PROPERTY_PROMO_VALUE']):?>showroom__cat-item--promo<?endif;?>" title="<?=$arFields['NAME']?>">
 				<span class="showroom__cat-item-name"><?=$arFields['NAME']?></span>
